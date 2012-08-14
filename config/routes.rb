308315b61058cfcb019541e255995424fa78ec87
Troy Shu <tmshu1@gmail.com>
2012-08-14 13:23:12 -0400

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 SampleApp::Application.routes.draw do
-  resources :users #adds shit like users/new, users/1, users/1/edit, etc.
+  #resources :users #adds shit like users/new, users/1, users/1/edit, etc.
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
-
+  resources :relationships, only: [:create, :destroy]
 
 
   root to:'static_pages#home'
